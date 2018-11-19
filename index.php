4d7a144840e73fa5af4a3294df7c59ed9163d642
khoursaaf <khoursa@gmail.com>
2018-11-19 10:16:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercise</title>
+</head>
+<body>
+  <p>En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.</p>
+  <?php
+  for ($a = 1; $a <= 15; $a++)
+  {
+    echo "On y arrive presque."."<br />";
+  }
+  ?>
+</body>
+</html>
