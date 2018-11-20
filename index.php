7d39b08362aaad1df53b81a07115b3a79840814f
khoursaaf <khoursa@gmail.com>
2018-11-20 16:26:28 +0100

    correction

--- index.php
+++ index.php
@@ -6,11 +6,7 @@
 </head>
 <body>
   <p>En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.</p>
-  <?php
-  for ($a = 1; $a <= 15; $a++)
-  {
-    echo "On y arrive presque."."<br />";
-  }
-  ?>
+  <?php for ($a = 1; $a <= 15; $a++) { ?>
+  <p><?php echo $a.' '.'On y arrive presque'; }?></p>
 </body>
 </html>
